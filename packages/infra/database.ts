3abc24e7f753e9468d48f0a12f65bcd4aa131512
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-11 19:38:05 -0300

    fix(database): fix database with a path error on sqlite file
    re #43

--- packages/infra/database.ts
+++ packages/infra/database.ts
@@ -1,5 +1,6 @@
 import sqlite from 'sqlite3';
 import logger from './logger';
+import path from 'node:path';
 
-const database: sqlite.Database = new sqlite.Database('database.sqlite', (err: Error | null) => logger.error(err));
+const database: sqlite.Database = new sqlite.Database(path.join(__dirname, 'database.sqlite'),(err: Error | null) => logger.error(err));
 export default database;
