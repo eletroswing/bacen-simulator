7892c2fe5cf88314232e7e6c1f5e2ad3ad6d0114
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-12 19:09:35 -0300

    refactor(zodvalidator): bring a new validator to get the request itself instead body

--- apps/api/util/zodValidator.ts
+++ apps/api/util/zodValidator.ts
@@ -1,16 +1,40 @@
+import { FastifyRequest } from "fastify";
 import z, { Schema } from "zod";
 
 function parse(schema: Schema): Function {
-  return (data: any): { err: unknown | null, data: z.infer<typeof schema> | null } => {
-    const parsed:any  = schema.safeParse(data);
+  return (req: FastifyRequest): { err: unknown | null, data: z.infer<typeof schema> | null } => {
+    if (!["text/xml", "application/xml", "application/rss+xml"].includes(req.headers['content-type'] as string)) {
+      return {
+        err: {
+          problem: {
+            "@xmlns": "urn:ietf:rfc:7807",
+            type: "https://dict.pi.rsfn.net.br/api/v2/error/EntryInvalid",
+            title: "Entry is invalid",
+            status: 400,
+            detail: "Entry has invalid fields",
+            violations: [
+              {
+                violation: {
+                  reason: "Entry has invalid body",
+                  value: `${req.body || 'No body detected'}`,
+                  property: "entry"
+                }
+              }
+            ]
+          }
+        }, data: null
+      }
+    }
+
+    const parsed: any = schema.safeParse(req.body);
     if (parsed.success) {
-      return { err: null, data: data };
+      return { err: null, data: req.body };
     }
 
     const json_err = JSON.parse(parsed.error.message);
 
-    let value = data;
-    for(let i = 0; i < json_err[0].path.length; i++) {
+    let value: any = req.body;
+    for (let i = 0; i < json_err[0].path.length; i++) {
       value = value[json_err[0].path[i]]
     };
 
