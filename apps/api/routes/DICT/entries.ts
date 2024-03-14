82a6a8432fd1cc77ccd316545e9a098fc7721d7b
YanDevv <ybritogomes@gmail.com>
2024-03-14 19:05:32 -0300

    feat(updateKey): assigned updateKey function to the endpoint

--- apps/api/routes/DICT/entries.ts
+++ apps/api/routes/DICT/entries.ts
@@ -2,13 +2,14 @@ import { FastifyInstance, FastifyReply, FastifyRequest, RegisterOptions } from '
 
 import createKey from '@api/services/DICT/entries/createKey';
 import getKey from '@api/services/DICT/entries/getKey';
+import updateKey from '@api/services/DICT/entries/updateKey';
 
 export default (instance: FastifyInstance, _opts: RegisterOptions, done: Function) => {
     instance.get('/:key', getKey);
 
     instance.post('/', createKey);
 
-    instance.put('/:key', (req: FastifyRequest, res: FastifyReply) => { res.send('Must update the key') });
+    instance.put('/:key', updateKey);
     
     instance.post('/:key/delete', (req: FastifyRequest, res: FastifyReply) => { res.send('Must delete the key') });
     done();
