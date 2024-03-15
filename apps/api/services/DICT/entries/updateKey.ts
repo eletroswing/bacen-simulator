d6aaa3c2d6d429dc9e941180e5003d97eec2bbb6
Yan Victor <ybritogomes@gmail.com>
2024-03-15 08:58:45 -0300

    fix(updateKey): fixed updateSql pathing

--- apps/api/services/DICT/entries/updateKey.ts
+++ apps/api/services/DICT/entries/updateKey.ts
@@ -59,6 +59,7 @@ export default async (req: FastifyRequest<{
         const queryOwner = `SELECT * FROM tb_Owners WHERE taxIdNumber = ?`;
         const queriedOwner = await database.get_sync(queryOwner, [queriedEntry.taxIdNumber]);
 
+        // TODO: Analyze the best method to query the update
         if(queriedOwner.type === 'NATURAL_PERSON') {
             // const updateAccounts = `UPDATE tb_Accounts SET participant = ?, branch = ?, accountType = ?, openingDate = ? WHERE accountNumber = ?`;
             // const updateOwners = `UPDATE tb_Owners SET name = ? WHERE taxIdNumber = ?`;
@@ -66,12 +67,12 @@ export default async (req: FastifyRequest<{
             // const updateOwnersParams = [parsed_body.data?.UpdateEntryRequest.Owner.Name, queriedOwner.taxIdNumber];
             // const accountsSql = await database.get_sync(updateAccounts, updateAccountsParams);
             // const ownersSql = await database.get_sync(updateOwners, updateOwnersParams);
-            const updateSql = `UPDATE tb_Accounts SET participant = ${parsed_body.data?.UpdateEntryRequest.Account.Participant}, branch = ${parsed_body.data?.UpdateEntryRequest.Account.Branch}, accountType = ${parsed_body.data?.UpdateEntryRequest.Account.AccountType}, openingDate = ${parsed_body.data?.UpdateEntryRequest.Account.OpeningDate} WHERE accountNumber = ${queriedEntry.accountNumber};
-            UPDATE tb_Owners SET type=${parsed_body.data?.UpdateEntryRequest.Owner.Type}, name = ${parsed_body.data?.UpdateEntryRequest.Owner.Name} WHERE taxIdNumber = ${queriedOwner.taxIdNumber}`;
+            const updateSql = `UPDATE tb_Accounts SET participant = "${parsed_body.data?.UpdateEntryRequest.Account.Participant}", branch = "${parsed_body.data?.UpdateEntryRequest.Account.Branch}", accountType = "${parsed_body.data?.UpdateEntryRequest.Account.AccountType}", openingDate = "${parsed_body.data?.UpdateEntryRequest.Account.OpeningDate}" WHERE accountNumber = "${queriedEntry.accountNumber}";
+            UPDATE tb_Owners SET type = "${parsed_body.data?.UpdateEntryRequest.Owner.Type}", name = "${parsed_body.data?.UpdateEntryRequest.Owner.Name}" WHERE taxIdNumber = "${queriedOwner.taxIdNumber}"`;
             const updatedSql = await database.get_sync(updateSql, []);
         }else {
-            const updateSql = `UPDATE tb_Accounts SET participant = ${parsed_body.data?.UpdateEntryRequest.Account.Participant}, branch = ${parsed_body.data?.UpdateEntryRequest.Account.Branch}, accountType = ${parsed_body.data?.UpdateEntryRequest.Account.AccountType}, openingDate = ${parsed_body.data?.UpdateEntryRequest.Account.OpeningDate} WHERE accountNumber = ${queriedEntry.accountNumber};
-            UPDATE tb_Owners SET type=${parsed_body.data?.UpdateEntryRequest.Owner.Type}, name = ${parsed_body.data?.UpdateEntryRequest.Owner.Name}, tradeName=${parsed_body.data?.UpdateEntryRequest.Owner.TradeName} WHERE taxIdNumber = ${queriedOwner.taxIdNumber}`;
+            const updateSql = `UPDATE tb_Accounts SET participant = "${parsed_body.data?.UpdateEntryRequest.Account.Participant}", branch = "${parsed_body.data?.UpdateEntryRequest.Account.Branch}", accountType = "${parsed_body.data?.UpdateEntryRequest.Account.AccountType}", openingDate = "${parsed_body.data?.UpdateEntryRequest.Account.OpeningDate}" WHERE accountNumber = "${queriedEntry.accountNumber}";
+            UPDATE tb_Owners SET type = "${parsed_body.data?.UpdateEntryRequest.Owner.Type}", name = "${parsed_body.data?.UpdateEntryRequest.Owner.Name}", tradeName = "${parsed_body.data?.UpdateEntryRequest.Owner.TradeName}" WHERE taxIdNumber = "${queriedOwner.taxIdNumber}"`;
             const updatedSql = await database.get_sync(updateSql, []);
         }
 
