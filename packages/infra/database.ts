f1ee394166df3605dd56240d16f58ac3c722fb98
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-15 19:59:25 -0300

    feat(database): add exec sync

--- packages/infra/database.ts
+++ packages/infra/database.ts
@@ -7,10 +7,24 @@ declare module 'sqlite3' {
         get_sync: (sql: string, params: any) => Promise<any>;
         run_sync: (sql: string, params: any) => Promise<any>;
         get_multiple_sync: (sql: string, params: any) => Promise<any>;
+        exec_sync: (sql: string) => Promise<any>;
     }
 }
 
 const database: sqlite.Database = new sqlite.Database(path.join(__dirname, 'database.sqlite'), (err: Error | null) => logger.error(err));
+
+database.exec_sync = async (sql: string): Promise<any> => {
+    return await new Promise((resolve, reject) => {
+        database.exec(sql, (err: Error | null) => {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(true)
+        })
+    })
+
+};
 
 database.get_sync = async (sql: string, params: any): Promise<any> => {
     return await new Promise((resolve, reject) => {
