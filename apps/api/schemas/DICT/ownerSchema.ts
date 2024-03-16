2ad251a664a49818f85dce0d31ae5737ad057b99
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-16 20:55:02 -0300

    refactor(ownerschema): refactor owner schema to improve the meaning

--- apps/api/schemas/DICT/ownerSchema.ts
+++ apps/api/schemas/DICT/ownerSchema.ts
@@ -1,78 +1,24 @@
 import { z } from 'zod';
 
-function Evaluate(assert: boolean) {
-	if (assert) return undefined;
-	return true;
-}
-
 export default z
 	.object({
 		Type: z.enum(['NATURAL_PERSON', 'LEGAL_PERSON']),
 		TaxIdNumber: z.coerce.string().max(14),
 		Name: z.coerce.string().max(120),
 		TradeName: z.coerce.string().nullable(),
+	}).superRefine((data, ctx) => {
+		switch (data.Type) {
+			case 'NATURAL_PERSON':
+				if (data.TradeName !== 'undefined') ctx.addIssue({ code: z.ZodIssueCode.custom, message: 'TradeName is not required when Type is equal to NATURAL_PERSON.', path: ['TradeName'] })
+				else if (!/^[0-9]{11}$/.test(data.TaxIdNumber)) ctx.addIssue({ code: z.ZodIssueCode.custom, message: 'TradeName is required when Type is equal to LEGAL_PERSON.', path: ['TradeName'] })
+				else if (!/^([A-Za-zÀ-ÖØ-öø-ÿ' -]+)$/.test(data.Name)) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Value does not match regex \'/^([A-Za-zÀ-ÖØ-öø-ÿ\' -]+)$/\'", path: ['Name'] })
+				else return true
+			case 'LEGAL_PERSON':
+				if (data.TradeName === 'undefined') ctx.addIssue({ code: z.ZodIssueCode.custom, message: 'TradeName is required when Type is equal to LEGAL_PERSON.', path: ['TradeName'] })
+				else if (!/^[0-9]{14}$/.test(data.TaxIdNumber)) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Value does not match regex \'/^[0-9]{14}$/\'", path: ['TaxIdNumber'] })
+				else if (!/^([A-Za-zÀ-ÖØ-öø-ÿ,.@:&*+_<>()!?/\\$%\d' -]+)$/.test(data.TaxIdNumber)) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Value does not match regex \'/^([A-Za-zÀ-ÖØ-öø-ÿ,.@:&*+_<>()!?/\\$%d\' -]+)$/\'", path: ['Name'] })
+				else return true
+			default:
+				return true;
+		}
 	})
-	.refine(
-		(data) =>
-			Evaluate(
-				data.Type === 'NATURAL_PERSON' && data.TradeName !== 'undefined',
-			),
-		{
-			message:
-				'TradeName is not required when Type is equal to NATURAL_PERSON.',
-			path: ['TradeName'],
-		},
-	)
-	.refine(
-		(data) =>
-			Evaluate(data.Type === 'LEGAL_PERSON' && data.TradeName === 'undefined'),
-		{
-			message: 'TradeName is required when Type is equal to LEGAL_PERSON.',
-			path: ['TradeName'],
-		},
-	)
-	.refine(
-		(data) =>
-			Evaluate(
-				data.Type === 'NATURAL_PERSON' && !/^[0-9]{11}$/.test(data.TaxIdNumber),
-			),
-		{
-			message: "Value does not match regex '/^[0-9]{11}$/'",
-			path: ['TaxIdNumber'],
-		},
-	)
-	.refine(
-		(data) =>
-			Evaluate(
-				data.Type === 'LEGAL_PERSON' && !/^[0-9]{14}$/.test(data.TaxIdNumber),
-			),
-		{
-			message: "Value does not match regex '/^[0-9]{14}$/'",
-			path: ['TaxIdNumber'],
-		},
-	)
-	.refine(
-		(data) =>
-			Evaluate(
-				data.Type === 'NATURAL_PERSON' &&
-					!/^([A-Za-zÀ-ÖØ-öø-ÿ' -]+)$/.test(data.Name),
-			),
-		{
-			message: "Value does not match regex '/^([A-Za-zÀ-ÖØ-öø-ÿ' -]+)$/'",
-			path: ['Name'],
-		},
-	)
-	.refine(
-		(data) =>
-			Evaluate(
-				data.Type === 'LEGAL_PERSON' &&
-					!/^([A-Za-zÀ-ÖØ-öø-ÿ,.@:&*+_<>()!?/\\$%\d' -]+)$/.test(
-						data.TaxIdNumber,
-					),
-			),
-		{
-			message:
-				"Value does not match regex '/^([A-Za-zÀ-ÖØ-öø-ÿ,.@:&*+_<>()!?/\\$%d' -]+)$/'",
-			path: ['Name'],
-		},
-	);
