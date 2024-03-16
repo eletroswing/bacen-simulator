99eced032a30bf431465273bdd7d5137a7c4f02e
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-15 22:12:15 -0300

    feat(openapi): removing swagger command cause serve do it automatic now

--- apps/api/SConfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    openapi: '3.1.0',
-    info: {
-        title: 'Bacen Simulator',
-        version: '0.0.1',
-        description: 'Documentation for a Bacen Simulator',
-    },
-}
