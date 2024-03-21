04a8e948498c65238a5fd91a3be3312582c5f278
eletroswing <ytalosilvabatalha@gmail.com>
2024-03-21 20:52:42 -0300

    docs(swagger): implement scalar ui instead of swagger default ui

--- apps/api/plugins/SwaggerJsDocsUiPlugin.ts
+++ apps/api/plugins/SwaggerJsDocsUiPlugin.ts
@@ -34,33 +34,13 @@ function SwaggerJsDocsUiPlugin(params?: {
 		);
 
 		fastify.get(
-			'/swagger-ui.css',
-			(_request: FastifyRequest, reply: FastifyReply) => {
-				reply
-					.type('text/css')
-					.send(fs.readFileSync(path.join(swaggerPath(), 'swagger-ui.css')));
-			},
-		);
-
-		fastify.get(
-			'/swagger-ui-bundle.js',
-			(_request: FastifyRequest, reply: FastifyReply) => {
-				reply
-					.type('application/javascript')
-					.send(
-						fs.readFileSync(path.join(swaggerPath(), 'swagger-ui-bundle.js')),
-					);
-			},
-		);
-
-		fastify.get(
-			'/swagger-ui-standalone-preset.js',
+			'/api-reference.js',
 			(_request: FastifyRequest, reply: FastifyReply) => {
 				reply
 					.type('application/javascript')
 					.send(
 						fs.readFileSync(
-							path.join(swaggerPath(), 'swagger-ui-standalone-preset.js'),
+							path.join(__dirname, 'files', 'api-reference.js'),
 						),
 					);
 			},
@@ -78,49 +58,30 @@ function SwaggerJsDocsUiPlugin(params?: {
 		fastify.get(
 			params?.route || '/docs',
 			(_request: FastifyRequest, reply: FastifyReply) => {
-				reply.type('text/html').send(`<!DOCTYPE html>
-            <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Swagger UI Example</title>
-              <!-- Load Swagger UI CSS -->
-              <link rel="stylesheet" type="text/css" href="/swagger-ui.css">
-              <style>
-                html, body {
-                  margin: 0;
-                  padding: 0;
-                  height: 100%;
-                }
-                #swagger-ui {
-                  height: 100%;
-                }
-              </style>
-            </head>
-            <body>
-              <!-- Add the Swagger UI container -->
-              <div id="swagger-ui"></div>
-            
-              <!-- Load Swagger UI JavaScript -->
-              <script src="swagger-ui-bundle.js"></script>
-              <script src="swagger-ui-standalone-preset.js"></script>
-            
-              <script>
-                // Initialize Swagger UI
-                const ui = SwaggerUIBundle({
-                  dom_id: '#swagger-ui',
-                  url: '/swagger.json', // Replace this with your Swagger JSON URL
-                  presets: [
-                    SwaggerUIBundle.presets.apis,
-                    SwaggerUIStandalonePreset
-                  ],
-                  layout: 'BaseLayout',
-                });
-              </script>
-            </body>
-            </html>
-            
-`);
+				reply.type('text/html').send(`<!doctype html>
+				<html>
+				  <head>
+					<title>API Reference</title>
+					<meta charset="utf-8" />
+					<meta
+					  name="viewport"
+					  content="width=device-width, initial-scale=1" />
+				  </head>
+				  <body>
+					<script
+					  id="api-reference"
+					  data-url="swagger.json"></script>
+					<script>
+					  var configuration = {
+						theme: 'purple',
+					  }
+				
+					  var apiReference = document.getElementById('api-reference')
+					  apiReference.dataset.configuration = JSON.stringify(configuration)
+					</script>
+					<script src="/api-reference.js"></script>
+				  </body>
+				</html>`);
 			},
 		);
 
